a14626a37ae3595aeec5e2b07aad54493681a721
Lorraine Morrissey <lorraine.morrissey@atu.ie>
2025-02-21 15:16:36 +0000

    Update format

--- Week2/bank.py
+++ Week2/bank.py
@@ -2,7 +2,7 @@
 #Week 2 Task - Prompt user to input 2 amounts in cents add the amounts together and print out in readable format
 #Author: Lorraine Morrissey
 
-amount1 = int(input("Enter amount1(in cents): "))
-amount2 = int(input("Enter amount2(in cents):"))
+amount1 = int(input("Enter amount 1(in cents): "))
+amount2 = int(input("Enter amount 2(in cents):"))
 totalamount = ((amount1 + amount2)/100)
 print ("The sum of these is €"+ f"{totalamount}")

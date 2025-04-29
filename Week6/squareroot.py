cea97ee7a11182f7a9dd9a45a2c9fc08231689b0
Lorraine Morrissey <lorraine.morrissey@atu.ie>
2025-04-29 22:39:47 +0100

    Week 6 task

---
+++ Week6/squareroot.py
@@ -0,0 +1,12 @@
+# squareroot.py
+# Week 6 Task -  create a square root function  
+
+# Reference Material
+# 
+# 
+
+# Author: Lorraine Morrissey
+
+number = float(input("Please enter a positive number: "))
+
+print(number)
